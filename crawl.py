d0e7c2592fe1849db23d4f7ee4ecdc959c5892c8
Huh <Lucas.Huh@startrack.com.au>
2020-06-08 19:03:13 +1000

    2.5day test

--- crawl.py
+++ crawl.py
@@ -2,39 +2,24 @@ from selenium import webdriver
 from time import sleep
 
 PATH = r'C:\Program Files\tmp\chromedriver.exe'
+
+with open (r'C:\Users\huhl\Desktop\devops\portfolio\test2.csv', 'w') as createcsv:
+    createcsv.write("Day, Scheduled, Tracked \n")
+
 driver = webdriver.Chrome(PATH)
 
 driver.get('https://www.flightradar24.com/data/airports/jfk/statistics')
 driver.execute_script("window.scrollTo(0,600)")
-sleep(5)
+sleep(3)
 driver.find_element_by_class_name("highcharts-exporting-group").click()
 ul = driver.find_element_by_class_name("highcharts-menu")
 ul.find_elements_by_tag_name("li")[-1].click()
 
 
-# driver = webdriver.Chrome()
-# driver.get('https://www.flightradar24.com/data/airports/jfk/statistics')
-# driver.execute_script("window.scrollTo(0,600)")
-# sleep(5)
-# driver.find_element_by_class_name("highcharts-exporting-group").click()
+table_list = driver.find_elements_by_xpath('//div[@class="highcharts-data-table"]')
 
-# import bs4
-# import requests
-# from bs4 import BeautifulSoup
-#
-#
-# r = requests.get('https://www.flightradar24.com/data/airports/jfk/statistics')
-# soup = bs4.BeautifulSoup(r.text, "xml")
-#
-# soup
-# # soup.find_all('div', {"class:" 'highcharts-data-table'})
-# urlstat = 'https://www.flightradar24.com/data/airports/jfk/statistics'
-# urlreq = urllib.request.Request(urlstat, headers ={'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
-# urlpage = urllib.request.urlopen (urlreq)
-# #webresponse = urlopen('https://www.flightradar24.com/data/airports/jfk/statistics')
-# soup_mod = BeautifulSoup(urlpage, 'html.parser')
-#
-# tableprint = soup_mod.find("div", class_="highcharts-data-table")
-#
-#
-# print (tableprint)
+num_of_flights = len(table_list)
+
+with open (r'C:\Users\huhl\Desktop\devops\portfolio\test2.csv', 'a') as createcsv:
+        for iteration_list in range(num_of_flights):
+            createcsv.write(table_list[iteration_list].text)
